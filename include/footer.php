f686cfe9e2645ea679e1674366d106968abaf15a
山内満帆 <yamahokkjk915@icloud.com>
2025-11-24 13:55:54 +0900

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,17 @@
+<!--   フッター   -->
+<footer class="l-footer">
+  <ul class="l-footer__item">
+    <li class="l-footer__item-list">
+      <a class="l-footer__item__tel__link" href="/achievements/detail01.php">
+        健生グループサイト</a>
+    </li>
+  </ul>
+  <div class="l-footer__copy">
+    <p class="l-footer__copy-text">&copy; <span id="year"></span> yamauchi maho</p>
+    <script>
+    document.getElementById('year').textContent = new Date().getFullYear();
+  </script>
+  </div>
+</footer>
+
+<!--   / フッター   -->
